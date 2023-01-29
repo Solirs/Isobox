2d27b996e7416cf517b8fd22db788f77bc023b18
Solirs <zcauchemar@gmail.com>
2023-01-29 18:16:12 +0100

    copy /etc/resolv.conf to chroot

--- src/shellscripts/mounts.sh
+++ src/shellscripts/mounts.sh
@@ -5,4 +5,5 @@ mount -o bind /dev/pts $1/dev/pts
 mount -o bind /run $1/run
 mount -o bind /tmp $1/tmp
 mount -t sysfs /sys $1/sys
+cp -r /etc/resolv.conf $1/etc/resolv.conf
 cp /run/user/1000/gdm/Xauthority $1/root/.Xauthority
