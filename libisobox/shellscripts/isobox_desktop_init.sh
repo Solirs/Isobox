001cb8ba6ba1af1a1212dcb17ab155fb0dedd3be
Solirs <zcauchemar@gmail.com>
2023-02-02 00:20:32 +0100

    Add a few lines i accidentally deleted

--- libisobox/shellscripts/isobox_desktop_init.sh
+++ libisobox/shellscripts/isobox_desktop_init.sh
@@ -2,7 +2,9 @@
 
 if command -v "pipewire" &> /dev/null && command -v "pipewire-pulse" &> /dev/null
 then
-
+    #XDG Runtime dir has to be set for this to work
+    unset XDG_RUNTIME_DIR
+    export XDG_RUNTIME_DIR=$(mktemp -d /tmp/$(id -u)-runtime-dir.XXX)
     pipewire &
     pipewire-pulse &
     dbus-launch wireplumber &
