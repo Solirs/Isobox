c9675609ae6f47b16860ee1ddf69ffb027badcbb
Solirs <zcauchemar@gmail.com>
2023-02-03 22:46:25 +0100

    isobox_kill_all_children should log

--- libisobox/shellscripts/isobox_kill_all_children.sh
+++ libisobox/shellscripts/isobox_kill_all_children.sh
@@ -1,4 +1,5 @@
 #Kill the PIDs of all processes in the chroot but spare the PID that was passed as well as its parent process
 #$1 Is likely to be the pid of the main session when isobox is started being passed when it is exited
 #(See chroot.sh for example)
+echo "Killing all processes in chroot..."
 kill $(ps --format pid |grep -o "[0-9]*"|grep -wv $1|grep -wv $(ps -p $1 -o ppid=))
