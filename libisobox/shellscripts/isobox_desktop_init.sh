03abe35f7061a3ea92fd067015dfb7b44e50ee0c
Solirs <zcauchemar@gmail.com>
2023-02-26 00:28:25 +0100

    pipewire should be started via dbus-launch

--- libisobox/shellscripts/isobox_desktop_init.sh
+++ libisobox/shellscripts/isobox_desktop_init.sh
@@ -5,8 +5,8 @@ then
     #XDG Runtime dir has to be set for this to work
     unset XDG_RUNTIME_DIR
     export XDG_RUNTIME_DIR=$(mktemp -d /tmp/$(id -u)-runtime-dir.XXX)
-    pipewire &
-    pipewire-pulse &
+    dbus-launch pipewire &
+    dbus-launch pipewire-pulse &
     dbus-launch wireplumber &
 elif command -v "pulseaudio" > /dev/null
 then
